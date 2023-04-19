f6fd9818e04608f96e0589c3ee24a70e7b68cf68
laromicas <laromicas@hotmail.com>
2023-04-18 19:34:40 -0500

    Added more files and things for base

--- src/datoso_seed_base/__init__.py
+++ src/datoso_seed_base/__init__.py
@@ -4,4 +4,4 @@ __init__.py
 __all__ = ["__version__", "__author__", "__description__"]
 __version__ = "0.0.1"
 __author__ = "Lacides Miranda"
-__description__ = "Datoso plugin for seed _base"
+__description__ = "Datoso plugin for seed Base"
