960ec12d86043cf189d946e0542d0fa6a8b2d311
laromicas <laromicas@hotmail.com>
2023-04-27 00:41:27 -0500

    Pumped version for deploy

--- src/datoso_seed_base/__init__.py
+++ src/datoso_seed_base/__init__.py
@@ -2,7 +2,7 @@
 __init__.py
 """
 __all__ = ["__version__", "__author__", "__description__"]
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 __author__ = "Lacides Miranda"
-__description__ = "Datoso plugin for seed Base"
+__description__ = "Seed Plugin scaffold for Datoso"
 __preffix__ = "Base"
